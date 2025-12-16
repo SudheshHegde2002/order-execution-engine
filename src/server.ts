7c9832ff2dfa6522281b7e7c3aa8c07ac0b38e9c
Sudhesh Hegde <hegdesud2002@gmail.com>
2025-12-16 16:35:45 +0530

    updated server start message

--- src/server.ts
+++ src/server.ts
@@ -15,7 +15,7 @@ async function start() {
   })
 
   await app.listen({ port: 3000 })
-  console.log('Server running on http://localhost:3000')
+  console.log('Server running')
 }
 
 start().catch(err => {
